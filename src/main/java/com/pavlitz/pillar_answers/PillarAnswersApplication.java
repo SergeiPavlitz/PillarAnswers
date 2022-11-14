2e1b674464d30d14f0192f23db82cea09eced499
MrWhite <sergeymalek23@gmail.com>
2022-11-14 15:49:38 +0500

    first commit

---
+++ src/main/java/com/pavlitz/pillar_answers/PillarAnswersApplication.java
@@ -0,0 +1,13 @@
+package com.pavlitz.pillar_answers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillarAnswersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PillarAnswersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pavlitz/pillar_answers/PillarAnswersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavlitz.pillar_answers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillarAnswersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
