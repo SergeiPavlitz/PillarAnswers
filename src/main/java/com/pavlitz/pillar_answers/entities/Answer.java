def05897066d629b2abf4cbc5eeb47f5d27cce70
MrWhite <sergeymalek23@gmail.com>
2022-11-16 16:22:32 +0500

    JsonIgnoreProperties annotation added to answer

--- src/main/java/com/pavlitz/pillar_answers/entities/Answer.java
+++ src/main/java/com/pavlitz/pillar_answers/entities/Answer.java
@@ -1,8 +1,11 @@
 package com.pavlitz.pillar_answers.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
 import java.sql.Date;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Entity
 @Table(name = "pillaranswers")
 public class Answer {
